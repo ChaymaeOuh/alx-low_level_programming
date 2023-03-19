46d37d1f33b19b2d47c3d1692902da9c7845ce6d
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-03-19 14:30:03 -0700

    comb of numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print comb of numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int n;
+for (m = 48; n <= 57; n++)
+{
+putchar(n);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
