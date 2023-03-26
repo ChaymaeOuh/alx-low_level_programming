816e8e43da13b4b4146e9324ef70def2ef0d5d2c
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-03-26 05:16:39 -0700

    comb of two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Print com of two numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 48; i <= 57; i++)
+{
+for (j = i; j <= 57; j++)
+{
+putchar(ij);
+if (i != 57 && j != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
