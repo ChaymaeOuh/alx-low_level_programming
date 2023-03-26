051b9cb4473a5276434aa6e810aae8a2af262013
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-03-26 15:23:29 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ *
+ *Return: table
+ */
+void times_table(void)
+{
+int i;
+int j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+_putchar(i * j + '0');
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
