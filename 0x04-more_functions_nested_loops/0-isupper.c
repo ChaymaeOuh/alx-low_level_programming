912e2697e6b0ddfcd27e9be5ce28039add813327
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-03-30 08:22:18 -0700

    Checks if a number is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if a letter is upper
+ * @x: The number
+ * Return: 1 if x is upper 0 else
+ */
+
+int _isupper(int x)
+{
+if (x >= 65 && x <= 90)
+{
+return (1);
+}
+return (0);
+}
+
