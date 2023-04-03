692636afcd96735d5da5fe8885db9a9c7b7415fd
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-04-03 15:14:46 -0700

    swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the values of ywo integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+int *c;
+*c = *a;
+*a = *b;
+*b = *c;
+}
+
