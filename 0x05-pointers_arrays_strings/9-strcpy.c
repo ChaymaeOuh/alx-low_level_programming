7af7d3d6fd6335fb1d7d16a41807915a3c271d02
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-04-07 04:56:16 -0700

    copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * *_strcpy - copies a string
+ * @dest: the string copied
+ * @src: the string to copy
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+int j = 0;
+while (src[j] != '\0')
+{
+j++;
+}
+for (i = 0; i <= j; i++)
+{
+dest[i] = src[i];
+}
+return (*dest);
+}
+
