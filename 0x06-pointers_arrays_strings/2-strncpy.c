8e1b980d17329f23e85cac6b97c5e82bb7b4642f
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-04-08 10:25:07 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,19 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; i < n; i++)
+int i = 0;
+int l = 0;
+while (src[i++])
+{
+l++;
+}
+for (i = 0; src[i] && i < n; i++)
 {
 dest[i] = src[i];
+}
+for (i = l; i < n; i++)
+{
+dest[i] = '\0';
 }
 return (dest);
 }
