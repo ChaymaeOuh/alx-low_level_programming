61ccc98174b243f1454ac65934327b3e3004e784
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-04-08 10:02:32 -0700

    copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: the first string
+ * @src: the second string
+ * @n: number of elements
+ *
+ * Return: the result
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+}
+
