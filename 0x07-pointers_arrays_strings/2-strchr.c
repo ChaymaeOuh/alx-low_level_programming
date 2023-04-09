da3503dcda2e83a22453bd58c8f00747532cfd94
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-04-09 14:02:28 -0700

    locate a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,15 @@
 char *_strchr(char *s, char c)
 {
 unsigned int i;
-
+char *p = NULL;
 for (i = 0; s[i] <= '\0'; i++)
 {
 if (s[i] == c)
 {
-return (s + i);
+p = s + i;
+break;
 }
 }
-return (NULL);
+return (p);
 }
 
