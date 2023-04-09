9d75fc62bf85ff6fe91314e30b2816ea5a95fa87
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-04-09 05:50:51 -0700

    1337 encoding

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,22 +8,16 @@
  */
 char *leet(char *s)
 {
-int i;
-int j = 0;
-while (s[j] != '\0')
-j++;
-for (i = 0; i < j; i++)
+char *p = s;
+while (*p != '\0')
 {
-if (s[i] == 'a' || s[i] == 'A')
-s[i] = '4';
-if (s[i] == 'e' || s[i] == 'E')
-s[i] = '3';
-if (s[i] == 'o' || s[i] == 'O')
-s[i] = '0';
-if (s[i] == 't' || s[i] == 'T')
-s[i] = '7';
-if (s[i] == 'l' || s[i] == 'L')
-s[i] = '1';
+char c = *p;
+*p++ = (c == 'a' || c == 'A') ? '4' :
+(c == 'e' || c == 'E') ? '3' :
+(c == 'o' || c == 'O') ? '0' :
+(c == 't' || c == 'T') ? '7' :
+(c == 'l' || c == 'L') ? '1' :
+c;
 }
 return (s);
 }
