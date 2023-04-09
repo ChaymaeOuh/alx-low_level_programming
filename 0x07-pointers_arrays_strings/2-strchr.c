cfe13c4493c6f1b2fd635a44f20fad5f0ba8d2fd
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-04-09 09:13:35 -0700

    locate a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ *
+ * Return: pointer of the character or NULL if not founded
+ */
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+unsigned int j = 0;
+char *p = NULL;
+while (s[j] != '\0')
+{
+j++;
+}
+for (i = 0; i < j; i++)
+{
+if (s[i] == c)
+{
+*p = s[i];
+break;
+}
+}
+return (p);
+}
+
