7227265b644c227e2395a2e471acbfad26c3bb39
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-04-20 11:25:28 -0700

    calculate the sum of all arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of all the parameters
+ * @n: the number of parameters
+ * @...: a variable number of parameters
+ *
+ * Return: the sum or 0 if n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+if (n == 0)
+return (0);
+
+va_list ap;
+unsigned int i, sum = 0;
+
+va_start(ap, n);
+
+for (i = 0; i < n; i++)
+sum += va_arg(ap, int);
+
+va_end(ap);
+
+return (sum);
+}
+
