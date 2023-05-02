8a028f6b047fab264d16f0e7f86e5d00d0cb9d29
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-05-02 04:15:04 -0700

    free linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the list to free
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+
+if (head == NULL)
+return (NULL);
+
+while (*head)
+{
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
+}
+
+*head = NULL;
+}
+
