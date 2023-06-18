18d8e3bacac455fc981fd838ccab87e3474f1e47
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-06-18 07:28:10 -0700

    return the sum of all the data of a doubly linked list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -9,16 +9,13 @@
 
 int sum_dlistint(dlistint_t *head)
 {
-dlistint_t *tmp;
+dlistint_t *tmp = head;
 int sum = 0;
 
-if (*head == NULL)
-return (0);
-
-while (tmp->next)
+while (tmp != NULL)
 {
-tmp = tmp->next;
 sum += tmp->n;
+tmp = tmp->next;
 }
 
 return (sum);
